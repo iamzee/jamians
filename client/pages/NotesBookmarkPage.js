50285dfe0cc8074193b9c6d0b1bdc46a1529a48a
iamzee <zeeshan99adeen@gmail.com>
2019-03-14 22:13:01 +0530

    added loader and notesNav to notesBookmarkPage

--- client/pages/NotesBookmarkPage.js
+++ client/pages/NotesBookmarkPage.js
@@ -5,17 +5,22 @@ import {isAuthenticated} from '../helpers/auth.helper';
 
 import NoteItem from '../components/NoteItem';
 import NavBar from '../components/NavBar';
+import Loader from '../components/Loader';
+import NotesNav from '../components/NotesNav';
 
 class NotesBookmarkPage extends React.Component {
   state = {
     notes: [],
+    noNotes: false,
   };
 
   componentDidMount () {
     const jwt = isAuthenticated ();
     getBookmarkedNotes (jwt.token, jwt.user._id).then (notes => {
-      console.log (notes);
       this.setState (() => ({notes}));
+      if (notes.length === 0) {
+        this.setState (() => ({noNotes: true}));
+      }
     });
   }
 
@@ -23,7 +28,18 @@ class NotesBookmarkPage extends React.Component {
     return (
       <div>
         <NavBar title={'Notes Maina'} />
-        {this.state.notes.map (note => <NoteItem key={note._id} note={note} />)}
+        <NotesNav />
+        {this.state.noNotes
+          ? <p>No notes</p>
+          : <div>
+              {this.state.notes.length === 0
+                ? <Loader />
+                : <div>
+                    {this.state.notes.map (note => (
+                      <NoteItem key={note._id} note={note} />
+                    ))}
+                  </div>}
+            </div>}
       </div>
     );
   }
