747ad3694918a6b3ec32b6ad7b10d010a69e5a7e
iamzee <zeeshan99adeen@gmail.com>
2019-03-19 18:03:35 +0530

    styled bookmark button

--- client/QuestionPaper/components/BookmarkButton.js
+++ client/QuestionPaper/components/BookmarkButton.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
 import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/styles';
 
 import {isAuthenticated} from '../../helpers/auth.helper';
 import {addBookmark, removeBookmark} from '../../api/questionPaper.api';
 
+const styles = theme => ({
+  button: {
+    color: theme.questionPaper.tertiary,
+    marginRight: theme.spacing.unit * 2,
+    '&:hover': {
+      backgroundColor: theme.questionPaper.tertiary,
+      color: '#fff',
+    },
+  },
+});
+
 class BookmarkButton extends React.Component {
   state = {
     bookmarked: false,
+    loading: false,
   };
 
   componentDidMount () {
@@ -24,32 +37,41 @@ class BookmarkButton extends React.Component {
   }
 
   onAddBookmark = () => {
+    this.setState (() => ({loading: true}));
+
     const {questionPaper} = this.props;
     const {user} = isAuthenticated ();
 
     addBookmark (questionPaper._id, user._id).then (() => {
-      this.setState (() => ({bookmarked: true}));
+      this.setState (() => ({bookmarked: true, loading: false}));
     });
   };
 
   onRemoveBookmark = () => {
+    this.setState (() => ({loading: true}));
+
     const {questionPaper} = this.props;
     const {user} = isAuthenticated ();
 
     removeBookmark (questionPaper._id, user._id).then (() => {
-      this.setState (() => ({bookmarked: false}));
+      this.setState (() => ({bookmarked: false, loading: false}));
     });
   };
 
   render () {
+    const {classes} = this.props;
     return (
       <div>
         {this.state.bookmarked
-          ? <Button onClick={this.onRemoveBookmark}>Remove Bookmark</Button>
-          : <Button onClick={this.onAddBookmark}>Add Bookmark</Button>}
+          ? <Button className={classes.button} onClick={this.onRemoveBookmark}>
+              {this.state.loading ? 'Removing...' : 'Remove Bookmark'}
+            </Button>
+          : <Button className={classes.button} onClick={this.onAddBookmark}>
+              {this.state.loading ? 'Adding...' : 'Adding Bookmark'}
+            </Button>}
       </div>
     );
   }
 }
 
-export default BookmarkButton;
+export default withStyles (styles) (BookmarkButton);
