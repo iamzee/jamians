ef364e03b29372275ce4ba534dbb33c70ce612be
iamzee <zeeshan99adeen@gmail.com>
2019-03-23 20:27:16 +0530

    setup circular progress to notes filter

--- client/Notes/components/NotesFilter.js
+++ client/Notes/components/NotesFilter.js
@@ -7,10 +7,13 @@ import DialogActions from '@material-ui/core/DialogActions';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {withStyles} from '@material-ui/core/styles';
 
 import {listDepartments} from '../../api/department.api';
 import {isAuthenticated} from '../../helpers/auth.helper';
+import {readDepartment} from '../../api/department.api';
+import {readCourse} from '../../api/course.api';
 
 const styles = theme => ({
   dialog: {
@@ -19,9 +22,14 @@ const styles = theme => ({
     textAlign: 'center',
   },
   textField: {
-    width: '90%',
+    width: 300,
     margin: 'auto',
     marginBottom: theme.spacing.unit * 2,
+  },
+  progress: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+    color: theme.notes.primary,
   },
 });
 
@@ -34,6 +42,10 @@ class NotesFilter extends React.Component {
     subject: '',
     course: '',
     open: false,
+    showCourseLoader: false,
+    showCourses: true,
+    showSubjectLoader: false,
+    showSubjects: true,
   };
 
   componentDidMount () {
@@ -60,12 +72,38 @@ class NotesFilter extends React.Component {
 
   onDepartmentChange = e => {
     const department = e.target.value;
-    this.setState (() => ({department}));
+    this.setState (() => ({
+      department,
+      showCourseLoader: true,
+      showCourses: false,
+    }));
+
+    readDepartment (department).then (({courses}) => {
+      this.setState (() => ({
+        courses,
+        showCourseLoader: false,
+        showCourses: true,
+        course: '',
+      }));
+    });
   };
 
   onCourseChange = e => {
     const course = e.target.value;
-    this.setState (() => ({course}));
+    this.setState (() => ({
+      course,
+      showSubjectLoader: true,
+      showSubjects: false,
+    }));
+
+    readCourse (course).then (({subjects}) => {
+      this.setState (() => ({
+        subjects,
+        showSubjectLoader: false,
+        showSubjects: true,
+        subject: '',
+      }));
+    });
   };
 
   onSubjectChange = e => {
@@ -113,37 +151,65 @@ class NotesFilter extends React.Component {
                 </MenuItem>
               ))}
             </TextField>
-            <TextField
-              className={classes.textField}
-              select
-              label="Course"
-              margin="normal"
-              value={this.state.course}
-              onChange={this.onCourseChange}
-            >
-              {this.state.courses.map (course => (
-                <MenuItem key={course._id} value={course._id}>
-                  {course.name}
-                </MenuItem>
-              ))}
-            </TextField>
-            <TextField
-              className={classes.textField}
-              select
-              label="Subject"
-              margin="normal"
-              value={this.state.subject}
-              onChange={this.onSubjectChange}
-            >
-              {this.state.subjects.map (subject => (
-                <MenuItem key={subject._id} value={subject._id}>
-                  {subject.name}
-                </MenuItem>
-              ))}
-            </TextField>
+
+            <br />
+
+            {this.state.showCourseLoader &&
+              <CircularProgress
+                className={classes.progress}
+                size={24}
+                variant="indeterminate"
+              />}
+
+            {this.state.showCourses &&
+              <div>
+                <TextField
+                  className={classes.textField}
+                  select
+                  label="Course"
+                  margin="normal"
+                  value={this.state.course}
+                  onChange={this.onCourseChange}
+                >
+                  {this.state.courses.map (course => (
+                    <MenuItem key={course._id} value={course._id}>
+                      {course.name}
+                    </MenuItem>
+                  ))}
+                </TextField>
+
+                <br />
+
+                {this.state.showSubjectLoader &&
+                  <CircularProgress
+                    className={classes.progress}
+                    size={24}
+                    variant="indeterminate"
+                  />}
+
+                {this.state.showSubjects &&
+                  <TextField
+                    className={classes.textField}
+                    select
+                    label="Subject"
+                    margin="normal"
+                    value={this.state.subject}
+                    onChange={this.onSubjectChange}
+                  >
+                    {this.state.subjects.map (subject => (
+                      <MenuItem key={subject._id} value={subject._id}>
+                        {subject.name}
+                      </MenuItem>
+                    ))}
+                  </TextField>}
+
+                <br />
+
+              </div>}
+
           </DialogContent>
           <DialogActions>
-            <Button>Cancel</Button>
+            <Button onClick={this.onClose}>Cancel</Button>
             <Button onClick={this.onApplyFilter}>Apply Filter</Button>
           </DialogActions>
         </Dialog>
