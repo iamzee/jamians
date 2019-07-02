16c71fea7112dad714db5fff0cf5b7d2f066bede
iamzee <zeeshan99adeen@gmail.com>
2019-07-02 14:10:31 +0530

    setup action button on profile page

--- client/Users/pages/Profile.js
+++ client/Users/pages/Profile.js
@@ -6,13 +6,11 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 import {withStyles} from '@material-ui/core/styles';
 
 import {isAuthenticated} from '../../helpers/auth';
-import {readUser} from '../../api/user';
-import {Link} from '@material-ui/core';
+import {readUser, sendFriendRequest} from '../../api/user';
 
 const styles = theme => ({
   root: {
@@ -20,7 +18,7 @@ const styles = theme => ({
     margin: 'auto',
     marginTop: theme.spacing.unit * 15,
     padding: theme.spacing.unit * 5,
-    [theme.breakpoints.down ('xs')]: {
+    [theme.breakpoints.down('xs')]: {
       marginTop: theme.spacing.unit * 15,
       padding: theme.spacing.unit * 2,
     },
@@ -33,81 +31,141 @@ const styles = theme => ({
     width: 150,
     height: 150,
   },
-  link: {
-    textDecoration: 'none',
-    flexDirection: 'column',
-  },
-  paper: {
-    marginTop: theme.spacing (2),
-    padding: theme.spacing (2),
-  },
-  itemList: {
-    display: 'flex',
-  },
-  item: {
-    marginRight: theme.spacing (2),
-    textAlign: 'center',
-  },
 });
 
 class Profile extends React.Component {
   state = {
     user: null,
+    sending: false,
+    sent: false,
   };
 
   componentDidMount = async () => {
-    const {token} = isAuthenticated ();
+    const {token} = isAuthenticated();
     const {userId} = this.props.match.params;
-    const user = await readUser (userId, token);
-    this.setState (() => ({user}));
+    const user = await readUser(userId, token);
+    this.setState(() => ({user}));
   };
 
-  render () {
+  onSendFriendRequest = async () => {
+    this.setState(() => ({sending: true}));
+    const {token} = isAuthenticated();
+    await sendFriendRequest(this.state.user._id, token);
+
+    this.setState(() => ({sending: false, sent: true}));
+  };
+
+  isFriend = () => {
+    const {user: loggedInUser} = isAuthenticated();
+    console.log('LOG', loggedInUser._id);
+    console.log('FrI', this.state.user._id);
+    const isFriend = this.state.user.friends.find(
+      friend => friend.toString() === loggedInUser._id.toString()
+    );
+
+    if (isFriend) {
+      return true;
+    }
+
+    return false;
+  };
+
+  isFriendRequestSent = () => {
+    const {user: loggedInUser} = isAuthenticated();
+    const isFriendRequestSent = loggedInUser.friendRequestsSent.find(
+      friend => friend.toString() === this.state.user._id.toString()
+    );
+
+    if (isFriendRequestSent) {
+      return true;
+    }
+
+    return false;
+  };
+
+  isFriendRequest = () => {
+    const {user} = this.state;
+    const {user: loggedInUser} = isAuthenticated();
+
+    const isFriendRequest = loggedInUser.friendRequestsReceived.find(
+      friend => friend.toString() === user._id.toString()
+    );
+
+    if (isFriendRequest) {
+      return true;
+    }
+
+    return false;
+  };
+
+  render() {
     const {classes} = this.props;
     const {user} = this.state;
+    const {user: loggedInUser} = isAuthenticated();
     return (
       <div className={classes.root}>
-        {this.state.user &&
+        {user && (
           <div>
             <Card className={classes.card}>
               <CardContent>
                 <Grid container spacing={3}>
                   <Grid item xs={12} sm={6}>
-                    {user.avatar
-                      ? <Avatar className={classes.avatar} src={user.avatar} />
-                      : <Avatar className={classes.avatar}>
-                          <PersonIcon />
-                        </Avatar>}
+                    {user.avatar ? (
+                      <Avatar className={classes.avatar} src={user.avatar} />
+                    ) : (
+                      <Avatar className={classes.avatar}>
+                        <PersonIcon />
+                      </Avatar>
+                    )}
                   </Grid>
                   <Grid item xs={12} sm={6}>
-                    <Typography>{user.name}</Typography>
+                    <Typography variant="h4">{user.name}</Typography>
+                    {loggedInUser._id.toString() === user._id.toString() ? (
+                      <Button variant="outlined" color="secondary">
+                        Edit Profile
+                      </Button>
+                    ) : (
+                      <React.Fragment>
+                        {this.isFriend() ? (
+                          <Button variant="outlined" color="secondary">
+                            Remove Friend
+                          </Button>
+                        ) : (
+                          <React.Fragment>
+                            {this.isFriendRequestSent() ? (
+                              <Button variant="outlined" color="secondary">
+                                Friend Request Sent
+                              </Button>
+                            ) : (
+                              <React.Fragment>
+                                {this.isFriendRequest() ? (
+                                  <Button variant="outlined" color="secondary">
+                                    Accept Friend Request
+                                  </Button>
+                                ) : (
+                                  <Button
+                                    variant="outlined"
+                                    color="secondary"
+                                    onClick={this.onSendFriendRequest}
+                                  >
+                                    Send Friend Request
+                                  </Button>
+                                )}
+                              </React.Fragment>
+                            )}
+                          </React.Fragment>
+                        )}
+                      </React.Fragment>
+                    )}
                   </Grid>
                 </Grid>
               </CardContent>
             </Card>
-
-            <Paper className={classes.paper}>
-              <Typography gutterBottom variant="h5" color="secondary">
-                About
-              </Typography>
-              <div className={classes.itemList}>
-                <div className={classes.item}>
-                  <Link to={`/users/${user._id}/follower`}>
-                    <Typography>23</Typography>
-                    <Typography>Followers</Typography>
-                  </Link>
-                </div>
-                <div className={classes.item}>
-                  <Typography>12</Typography>
-                  <Typography>Following</Typography>
-                </div>
-              </div>
-              <Divider />
-            </Paper>
-          </div>}
+          </div>
+        )}
       </div>
     );
   }
 }
 
-export default withStyles (styles) (Profile);
+export default withStyles(styles)(Profile);
--- client/api/user.js
+++ client/api/user.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const create = async data => {
   try {
-    await axios ({
+    await axios({
       method: 'post',
       url: '/api/users',
       data,
@@ -11,13 +11,13 @@ export const create = async data => {
       },
     });
   } catch (e) {
-    console.log (e.response);
+    console.log(e.response);
   }
 };
 
 export const listUsers = async token => {
   try {
-    const {data} = await axios ({
+    const {data} = await axios({
       method: 'get',
       url: '/api/users',
       headers: {
@@ -31,16 +31,30 @@ export const listUsers = async token => {
 
 export const readUser = async (userId, token) => {
   try {
-    const {data} = await axios ({
+    const {data} = await axios({
       method: 'get',
       url: `/api/users/${userId}`,
       headers: {
         Authorization: 'Bearer ' + token,
       },
     });
-    console.log (data);
+    console.log(data);
     return data;
   } catch (e) {
-    console.log (e.response);
+    console.log(e.response);
+  }
+};
+
+export const sendFriendRequest = async (userId, token) => {
+  try {
+    return await axios({
+      method: 'post',
+      url: `/api/users/${userId}/friendRequest`,
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+  } catch (e) {
+    console.log(e.response);
   }
 };
