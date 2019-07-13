9c280432479bbd58b557ded5155910f4ce7de865
iamzee <zeeshan99adeen@gmail.com>
2019-07-13 18:58:35 +0530

    delete upload routes and azure files on client

--- server/routes/upload.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {generateSAS} from '../controllers/upload.controller';
-
-const router = express.Router ();
-
-router.route ('/generateSAS').post (generateSAS);
-
-export default router;
--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ import departmentRoutes from './routes/department';
 import noteRoutes from './routes/notes';
 import userRoutes from './routes/user';
 import authRoutes from './routes/auth';
-import uploadRoutes from './routes/upload.route';
 import questionPaperRoutes from './routes/questionPaper';
 // import chatRoutes from './routes/chat.route';
 // import discussionRoutes from './routes/discussion.route';
@@ -23,35 +22,34 @@ import template from '../template';
 import socketio from './socketio/socketio';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_MLAB_URI);
+mongoose.connect (process.env.MONGODB_MLAB_URI);
 
-const app = express();
+const app = express ();
 
-app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use (express.static ('public'));
+app.use (bodyParser.json ());
 
-app.use('/', courseRoutes);
-app.use('/', subjectRoutes);
-app.use('/', departmentRoutes);
-app.use('/', noteRoutes);
-app.use('/', userRoutes);
-app.use('/', authRoutes);
-app.use('/', uploadRoutes);
-app.use('/', questionPaperRoutes);
+app.use ('/', courseRoutes);
+app.use ('/', subjectRoutes);
+app.use ('/', departmentRoutes);
+app.use ('/', noteRoutes);
+app.use ('/', userRoutes);
+app.use ('/', authRoutes);
+app.use ('/', questionPaperRoutes);
 // app.use ('/', chatRoutes);
 // app.use ('/', discussionRoutes);
-app.use('/', eventRoutes);
-app.use('/', notificationRoutes);
+app.use ('/', eventRoutes);
+app.use ('/', notificationRoutes);
 
-app.get('*', (req, res) => {
-  res.send(template());
+app.get ('*', (req, res) => {
+  res.send (template ());
 });
 
 const PORT = process.env.PORT || 3000;
 
-const expressServer = app.listen(PORT, () => {
-  console.log(`Server is up at port ${PORT}`);
+const expressServer = app.listen (PORT, () => {
+  console.log (`Server is up at port ${PORT}`);
 });
 
-const io = socketIO(expressServer);
-socketio(io);
+const io = socketIO (expressServer);
+socketio (io);
